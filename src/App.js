2b5ef41e530c79294c26b16779e313d3ad65bf69
Ivan Derlich <e@ivanderlich.com>
2020-09-13 22:43:31 -0300

    Edit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
-const title = "React"
-
-function App() {
-  return (
-    <div>
-      <h1>Hello {title}</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-  );
+function getTitle(title) {
+return title;
 }
-
+function App() {
+return (
+<div>
+<h1>Hello {getTitle('React')}</h1>
+<label htmlFor="search">Search: </label>
+<input id="search" type="text" />
+</div>
+);
+}
 export default App;
